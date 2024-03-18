540d4c277daac21dd97e2cc5791a012b74b935b5
Christoph Senkel <christoph.senkel@credativ.de>
2024-03-18 13:17:43 +0100

    improve function call

--- pve-ontap-snapshot.py
+++ pve-ontap-snapshot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from proxmoxer import ProxmoxAPI
-from proxmoxer import core
+from proxmoxer import core, ResourceException
 
 from netapp_ontap import HostConnection
 from netapp_ontap.resources import Volume, Snapshot, FileClone, CLI
@@ -45,7 +45,7 @@ class VM:
                 self.config = self.prox.nodes(node['node']).qemu(self.id).config.get()
                 self.node = node['node']
                 break
-            except core.ResourceException as e:
+            except ResourceException as e:
                 pass
         self.storages = []
         for key, value in self.config.items():
@@ -75,7 +75,14 @@ class VM:
     def start(self):
         self.prox.nodes(self.node).qemu(self.name).status.start.post()
 
-    def create(self):
+    def create(self, suspend=False, shutdown=False):
+        if suspend:
+            self.suspend()
+        if shutdown:
+            self.shutdown()
+        if self.status != 'stopped':
+            logging.warning('creating snapshot of a running vm, the result might be inconsistent')
+
         timestamp = strftime("%Y-%m-%d_%H:%M:%S+0000", gmtime())
         for storage in self.storages:
             volume = get_volume(storage.volume_name, storage.access)
@@ -94,13 +101,19 @@ class VM:
                                 storage.access['pass'],
                                 verify=storage.access['verify']):
                 file_clone.post()
+        if suspend or shutdown:
+            self.start()
 
 class Storage:
     def __init__(self, storage, config) -> None:
         logging.debug(f'Storage init parameters: {storage}, {dict(config)}')
         self.storage = storage
         self.prox = ProxmoxAPI(config['proxmox']['proxmox_host'], user=config['proxmox']['proxmox_user'], password=config['proxmox']['proxmox_pass'], verify_ssl=(True if config['proxmox']['proxmox_verify'].lower() == 'true' else False))
-        self.volume_name = self.prox.storage(storage).get()['export'].strip('/')
+        try:
+            self.volume_name = self.prox.storage(storage).get()['export'].strip('/')
+        except ResourceException as e:
+            print(e)
+            sys.exit(1)
         self.access = dict(config[storage.removesuffix('-CLONE')])
         self.access['verify'] = True if self.access['verify'].lower() == 'true' else False
         self.disk = ''
@@ -123,7 +136,7 @@ class Storage:
         snapshot = Snapshot.from_dict({
             "name": f'proxmox_snapshot_{timestamp}',
             "comment": f"Snapshot of Proxmox storage {self.storage}",
-            "volume": volume.to_dict()
+            "volume": {'name': volume.name, 'uuid': volume.uuid}
         })
         logging.debug(snapshot)
         with HostConnection(self.access['host'],
@@ -205,73 +218,23 @@ class Storage:
         volume = get_volume(self.volume_name, self.access)
         pprint(volume.to_dict())
 
-
-def vm_create(args):
-    '''
-        creates a snapshot from a running vm, optional suspended or shutdown
-        uses the ONTAP volume snapshot function
-        store vm info in snapshot name
-    '''
-    vm = VM(args.vm, config)
-    # vm.add_ontap_access(config)
-    logging.debug(str(vm))
-
-    start = False
-    if args.shutdown:
-        vm.shutdown()
-        start = True
-
-    if args.suspend:
-        vm.suspend()
-        start = True
-
-    if vm.status != 'stopped':
-        logging.warning('creating snapshot of a running vm, the result might be inconsistent')
-
-    vm.create()
-
-    if start:
-        vm.start()
-
-def storage_create(args):
-    storage = Storage(args.storage, config)
-    logging.debug(str(storage))
-    storage.create()
-
-def storage_restore(args):
-    storage = Storage(args.storage, config)
-    logging.debug(str(storage))
-    storage.restore(args.snapshot)
-
-def storage_delete(args):
-    storage = Storage(args.storage, config)
-    logging.debug(str(storage))
-    storage.delete(args.snapshot)
-
-def storage_list(args):
-    storage = Storage(args.storage, config)
-    logging.debug(str(storage))
-    storage.list()
-
-def storage_mount(args):
-    storage = Storage(args.storage, config)
-    logging.debug(str(storage))
-    storage.mount(args.snapshot)
-
-def storage_unmount(args):
-    storage = Storage(args.storage, config)
-    logging.debug(str(storage))
-    storage.unmount()
-
-def storage_show(args):
-    storage = Storage(args.storage, config)
-    logging.debug(str(storage))
-    storage.show()
+def caller(args):
+    logging.debug(args)
+    context = args.context((args.vm if 'vm' in args else args.storage), config)
+    logging.debug(str(context))
+    cmd = getattr(context, args.cmd)
+    parameters = {}
+    for arg, value in vars(args).items():
+        if arg not in ['config', 'loglevel', 'vm', 'storage', 'func', 'context', 'cmd']:
+            parameters[arg] = value
+    logging.debug(f'options: {parameters}')
+    cmd(**parameters)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser_main = parser.add_argument('-config', type=str, default='config.ini', help='Path to config file, default is "./config.ini"')
-    parser_main = parser.add_argument('-loglevel', choices=['info', 'warn', 'error', 'debug'], default='info', help='Set the loglevel, default is info')
+    parser.add_argument('-config', type=str, default='config.ini', help='Path to config file, default is "./config.ini"')
+    parser.add_argument('-loglevel', choices=['info', 'warn', 'error', 'debug'], default='info', help='Set the loglevel, default is info')
+    parser.set_defaults(func=caller)
     subparsers = parser.add_subparsers(title='Proxmox operations', required=True)
     subparser_vm = subparsers.add_parser('vm', help='Snapshots in VM context')
     subparser_storage = subparsers.add_parser('storage', help='Snapshots in Storage context')
@@ -281,33 +244,39 @@ if __name__ == '__main__':
     parser_vm_create.add_argument('-vm', type=int, required=True, help='Proxmox VM ID')
     parser_vm_create.add_argument('-suspend', type=bool, default=False, help='Suspend VM before creating the snapshot')
     parser_vm_create.add_argument('-shutdown', type=bool, default=False, help='Shutdown the VM before creating the snapshot')
-    parser_vm_create.set_defaults(func=vm_create)
+    parser_vm_create.set_defaults(context=VM, cmd='create')
 
     parser_storage = subparser_storage.add_subparsers(title='Storage actions', required=True)
     parser_storage_create = parser_storage.add_parser('create', help='Create a snapshot of the storage using ONTAP volume snapshot')
     parser_storage_create.add_argument('-storage', type=str, required=True, help='Proxmox Storage ID')
-    parser_storage_create.set_defaults(func=storage_create)
+    parser_storage_create.set_defaults(context=Storage, cmd='create')
+
     parser_storage_restore = parser_storage.add_parser('restore', help='Restore an ONTAP volume snapshot to the storage')
     parser_storage_restore.add_argument('-storage', type=str, required=True, help='Proxmox Storage ID')
     parser_storage_restore.add_argument('-snapshot', type=str, required=True, help='Snapshot to restore')
-    parser_storage_restore.set_defaults(func=storage_restore)
+    parser_storage_restore.set_defaults(context=Storage, cmd='restore')
+
     parser_storage_delete = parser_storage.add_parser('delete', help='Delete an ONTAP volume snapshot')
     parser_storage_delete.add_argument('-storage', type=str, required=True, help='Proxmox Storage ID')
     parser_storage_delete.add_argument('-snapshot', type=str, required=True, help='Snapshot to delete')
-    parser_storage_delete.set_defaults(func=storage_delete)
+    parser_storage_delete.set_defaults(context=Storage, cmd='delete')
+
     parser_storage_list = parser_storage.add_parser('list', help='List all ONTAP volume snapshots')
     parser_storage_list.add_argument('-storage', type=str, required=True, help='Proxmox Storage ID')
-    parser_storage_list.set_defaults(func=storage_list)
+    parser_storage_list.set_defaults(context=Storage, cmd='list')
+
     parser_storage_mount = parser_storage.add_parser('mount', help='Mount an ONTAP volume snapshot and add it as new storage to PVE')
     parser_storage_mount.add_argument('-storage', type=str, required=True, help='Proxmox Storage ID')
     parser_storage_mount.add_argument('-snapshot', type=str, required=True, help='Snapshot to mount')
-    parser_storage_mount.set_defaults(func=storage_mount)
+    parser_storage_mount.set_defaults(context=Storage, cmd='mount')
+
     parser_storage_unmount = parser_storage.add_parser('unmount', help='Unmount an ONTAP volume snapshot and remove its storage from PVE')
     parser_storage_unmount.add_argument('-storage', type=str, required=True, help='Proxmox Storage ID')
-    parser_storage_unmount.set_defaults(func=storage_unmount)
+    parser_storage_unmount.set_defaults(context=Storage, cmd='unmount')
+
     parser_storage_show = parser_storage.add_parser('show', help='Show metadata of the underlying ONTAP volume')
     parser_storage_show.add_argument('-storage', type=str, required=True, help='Proxmox Storage ID')
-    parser_storage_show.set_defaults(func=storage_show)
+    parser_storage_show.set_defaults(context=Storage, cmd='show')
 
     args = parser.parse_args()
 
